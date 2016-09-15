39d78b1730489f73ec0473478f9f28a06144d794
Martin Muzatko <martin@happy-css.com>
2016-09-15 11:36:28 +0200

    Created outline for a skill and triggers

--- src/js/engine/skill/skill.js
+++ src/js/engine/skill/skill.js
@@ -1,5 +1,30 @@
 export default class Skill {
-    constructor() {
+    constructor(name, effect) {
+        this.name = name
+        this.effect = effect
+        this.level = 1
+        this.cost = 5
+        this.delay = 5
+        riot.observable(this)
+        this.on('cast', (player) => {
+            this.cast(player)
+        })
+    }
 
+    levelUp() {
+        this.level += 1
+    }
+
+    canCast(player) {
+        return (this.cost < player.mana.amount)
+    }
+
+    cast(player) {
+        if (this.canCast(player)) {
+            player.mana.drain(this.cost*this.level)
+            this.effect()
+            
+            this.trigger('casted')
+        }
     }
 }
