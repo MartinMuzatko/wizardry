32bd650b5fc14637608f0996f01f938444b8055d
Martin Muzatko <martin@happy-css.com>
2016-09-15 11:35:26 +0200

    Updated player initial standards

--- src/js/engine/entity/player.js
+++ src/js/engine/entity/player.js
@@ -3,15 +3,18 @@ import Inventory from '../inventory/inventory'
 import SkillTree from '../skill/skilltree'
 import Resource from '../resource/resource'
 import Capacity from '../resource/capacity'
+import * as ITEMS from '../item/items'
 
 export default class Player {
     constructor() {
-        this.health = new Capacity(new Resource('health', 0),1000)
-        this.mana = new Capacity(new Resource('mana', 0),1000)
+        this.health = new Capacity(new Resource('health', 100),100)
+        this.mana = new Capacity(new Resource('mana', 20),100)
         this.position = new Position(2,3)
-        this.inventory = new Inventory(5)
+        this.inventory = new Inventory(10)
         this.skills = new SkillTree
         this.walking = 0
+        this.inventory.put(ITEMS.DIRT, 5)
+        console.log(this);
         riot.observable(this)
     }
 
