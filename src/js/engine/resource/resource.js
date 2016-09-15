f2bbe9ebc97143f768e1fa5bbb913a8751918afd
Martin Muzatko <martin@happy-css.com>
2016-09-15 11:35:55 +0200

    Extended resource methods

--- src/js/engine/resource/resource.js
+++ src/js/engine/resource/resource.js
@@ -1,10 +1,21 @@
 export default class Resource {
-    constructor(name, amount) {
+    constructor(name, amount=0) {
         this.name = name
         this.amount = amount
     }
 
-    canDrain() {}
-    drain() {}
-    gain() {}
+    canDrain(amount) {
+        return this.amount > amount
+    }
+
+    drain(amount) {
+        if (this.canDrain(amount)) {
+            this.amount -= amount
+            return true
+        }
+    }
+
+    gain() {
+
+    }
 }
